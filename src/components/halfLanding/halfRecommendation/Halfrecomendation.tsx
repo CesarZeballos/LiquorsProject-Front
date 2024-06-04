c081af7d3e1a13805fb82f0db4069cc5f970be26
tomas.avila <tomysykess@gmail.com>
2024-06-04 20:52:46 -0300

    transition

--- src/components/halfLanding/halfRecommendation/Halfrecomendation.tsx
+++ src/components/halfLanding/halfRecommendation/Halfrecomendation.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { IRecommendation } from "@/interfaces/interfaz";
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 
 const HalfRecommendation = ({
   recommendation,
@@ -9,7 +9,30 @@ const HalfRecommendation = ({
 }) => {
   const { title, imageB, imageF, imageP, description, color, link } =
     recommendation;
+  const [isVisible, setIsVisible] = useState(false);
   const [scrollY, setScrollY] = useState(0);
+  const ref = useRef(null);
+
+  useEffect(() => {
+    const observer = new IntersectionObserver(
+      (entries) => {
+        entries.forEach((entry) => {
+          setIsVisible(entry.isIntersecting);
+        });
+      },
+      { threshold: 0.1 }
+    );
+
+    if (ref.current) {
+      observer.observe(ref.current);
+    }
+
+    return () => {
+      if (ref.current) {
+        observer.unobserve(ref.current);
+      }
+    };
+  }, []);
 
   useEffect(() => {
     const handleScroll = () => {
@@ -25,24 +48,30 @@ const HalfRecommendation = ({
 
   return (
     <div
-      className={`relative w-1/3 h-full flex flex-col items-center justify-center overflow-hidden mt-20`}
+      ref={ref}
+      className={`relative w-1/3 h-full flex flex-col items-center justify-center overflow-hidden mt-20 ${
+        isVisible ? "opacity-100 transition-opacity duration-1000" : "opacity-0"
+      }`}
     >
       <img
         src={imageB}
         alt=""
-        className="absolute object-contain w-full h-[105%] z-0 opacity-95   "
+        className="absolute object-contain w-full h-[105%] z-0 opacity-95"
         style={{ transform: `translateY(-${scrollY * 0.1}px)` }}
       />
       <img
         src={imageP}
         alt=""
-        className="absolute object-contain w-full h-[105%] z-0 opacity-95   "
-        style={{ transform: `translateY(-${scrollY * 0.2}px)` }}
+        className="absolute object-contain w-full h-[105%] z-0 opacity-95"
+        style={{
+          transform: `translateY(-${scrollY * 0.5}px)`,
+          transition: "transform 3s ease",
+        }}
       />
       <img
         src={imageF}
         alt=""
-        className="absolute object-contain w-full h-[105%] z-0 opacity-95   "
+        className="absolute object-contain w-full h-[105%] z-0 opacity-95"
         style={{ transform: `translateY(-${scrollY * 0.5}px)` }}
       />
       <div className="relative z-10 text-base">
