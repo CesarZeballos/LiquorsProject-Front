8691e910dc4c1f6c208c2ea9c5ed1aa104b670b1
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-05 22:14:48 -0300

    -preparo codigo para postear login al backend (queda comentado) -cargo UID de login con google al localstorage

--- src/utils/loginFireBaseGoogle.ts
+++ src/utils/loginFireBaseGoogle.ts
@@ -1,11 +1,24 @@
+import axios from "axios";
+
 const loginUserFireBaseGoogle = async (auth: any, provider: any, router: any, setErrorGoogle:any, setIsLoadingGoogle: any, setIsSuccessGoogle: any, signInWithPopup: any) => {
     try {
         const result = await signInWithPopup(auth, provider);
-        console.log(result);
-        const user = JSON.stringify(result.user.accessToken);
-        localStorage.setItem("loginToken", user)
+        //LOGIN TOKEN
+        const userData = JSON.stringify(result.user.accessToken);
+        localStorage.setItem("loginToken", userData)
+        //UID 
+        const userDataUid = JSON.stringify(result.user.uid);
+        localStorage.setItem("uidFirebaseGoogleLogin", userDataUid)
+
         setErrorGoogle(null)
         setIsSuccessGoogle(true);
+
+        //___POST A BACK END________
+        //const response = await axios.post("https://liquors-project.onrender.com/users", userData )
+        // envio al backend UID y EMAIL que devuelve firebase.
+        //devuelve token el back, ese token me da permisos a la ruta de usuarios. con el token q da el back accedes a funciones de usuario.
+        //a la hora logear: solo le mando el uid al back.
+
         setTimeout(() => {
             router.push("/")
         })
