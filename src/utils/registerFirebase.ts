929e4219e8c19551fbf1aee1664375070064b416
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-05 22:14:00 -0300

    -prepare codigo para postear usuario al backend, lo dejo comentado.

--- src/utils/registerFirebase.ts
+++ src/utils/registerFirebase.ts
@@ -1,11 +1,20 @@
+import axios from "axios";
 
 const registerUserFirebase = async (formData: any, auth:any, createUserWithEmailAndPassword: any ,setIsSuccess: any, setErrors: any, router: any, errors: any, setIsLoading: any, setToken: any) => {
       try {
             const userCredential = await createUserWithEmailAndPassword(auth, formData.email, formData.password);
+            //DATA USUARIO REGISTRADO
             const user = userCredential.user; //Objeto que contiene información del usuario registrado. (token, mail, etc.)
             console.log('lo que devuelve firebase:', user);
+            
             setIsSuccess(true);
             setErrors({});
+
+            //___POST A BACK END________
+            //const response = await axios.post("https://liquors-project.onrender.com/users", user )
+            // envio al backend UID, EMAIL, PASSWORD que devuelve firebase.
+            //devuelve token el back, ese token me da permisos a la ruta de usuarios. con el token q da el back accedes a funciones de usuario.
+
             setTimeout(() => {
                   router.push("/login")
             }, 2000);
