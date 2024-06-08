8de3279dd59d959d89a98db8f7bb9a567e83054a
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-08 01:47:47 -0300

    -filtro del back gins y guardo en estado global

--- src/utils/getProductsGin.ts
+++ src/utils/getProductsGin.ts
@@ -1,15 +1,15 @@
 import axios from "axios";
 import { AppDispatch } from "@/store/store";
-import { readProducts } from "@/store/reducers/productsSlice";
-import { clearProducts } from "@/store/reducers/productsSlice";
+import { readGinProducts } from "@/store/reducers/productsSlice";
+import { clearGinProducts } from "@/store/reducers/productsSlice";
 import { Product } from "@/interfaces/interfaz";
 
 export const fetchProductsGin = async (dispatch: AppDispatch) => {
   try {
       const res = await axios.get<Product[]>("https://liquors-project.onrender.com/products/?category=Gin");
       //deberia hacer un estado global nuevo solo para products/gins.
-      dispatch(clearProducts());
-      dispatch(readProducts(res.data))
+      dispatch(clearGinProducts());
+      dispatch(readGinProducts(res.data));
   } catch (err) {
       console.error(err);
   }
