345a75e2548f584dc7bb03494a36cb439b352464
Juanch <Juanpablo_delrosso@outlook.com.ar>
2024-06-08 22:06:04 -0300

    -cambi

--- src/utils/getProductsFiltered.ts
+++ src/utils/getProductsFiltered.ts
@@ -5,11 +5,15 @@ import { Product } from "@/interfaces/interfaz";
 
 export const fetchProductsFiltered = async (dispatch: AppDispatch, filters: any, router: any) => {
   try {
+      //falta corregir el back para que reciba abv
       const abv = filters.abvRange
+
       const category = filters.categoryButton
+      
+      //falta logica en el back para rate
       const rate = filters.selectedRating
       console.log("valor de abv que envio a back", abv, "valor que envio al back de category",category);
-      const res = await axios.get<Product[]>(`https://liquors-project.onrender.com/products/?category=${category}&abv=${abv}`);
+      const res = await axios.get<Product[]>(`https://liquors-project.onrender.com/products/?category=${category}`);
       console.log("respuesta back", res);
       dispatch(clearProductsFiltered());
       dispatch(readProductsFiltered(res.data));
